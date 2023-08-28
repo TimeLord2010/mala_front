ef4559b556b4628ae0cabbb20fe2325470fe21ff
TimeLord2010 <timelord1025@gmail.com>
2023-08-28 15:22:19 -0300

    removed unnecesary comment

--- lib/usecase/patient/insert_patient.dart
+++ lib/usecase/patient/insert_patient.dart
@@ -4,5 +4,4 @@ import 'package:mala_front/models/patient.dart';
 Future<Patient> insertPatient(Patient patient) async {
   var rep = await createPatientRepository();
   return rep.insert(patient);
-  // TODO: handle index conflicts
 }
