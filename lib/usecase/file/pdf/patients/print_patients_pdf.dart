a0cd6307eebd0d09a37a0a54a6915b0248020a66
LAPTOP-52E7ENV9\João Victor <Joao Victor>
2023-10-13 09:01:57 -0300

    REFAC: print patients pdf

--- lib/usecase/file/pdf/patients/print_patients_pdf.dart
+++ lib/usecase/file/pdf/patients/print_patients_pdf.dart
@@ -1,5 +1,4 @@
 import 'package:mala_front/usecase/file/pdf/patients/create_patients_pdf.dart';
-import 'package:pdf/pdf.dart';
 import 'package:printing/printing.dart';
 
 import '../../../../models/patient.dart';
@@ -8,12 +7,8 @@ Future<void> printPatientsPdf({
   required List<Patient> patients,
 }) async {
   var bytes = await createPatientsPdf(patients: patients);
-  await Printing.layoutPdf(
-    onLayout: (format) {
-      return bytes;
-    },
-    name: 'Pacientes.pdf',
-    dynamicLayout: false,
-    format: PdfPageFormat.a4,
+  await Printing.sharePdf(
+    bytes: bytes,
+    filename: 'Pacientes.pdf',
   );
 }
