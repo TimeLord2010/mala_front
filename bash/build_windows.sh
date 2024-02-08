392b46ee3d2c1a00135f17627d9853f4fc6920b5
LAPTOP-52E7ENV9\João Victor <Joao Victor>
2024-02-08 13:52:08 -0300

    fix: windows build script

--- bash/build_windows.sh
+++ bash/build_windows.sh
@@ -1,6 +1,7 @@
 flutter build windows --release
 
-source_folder="./build/windows/runner/Release"
+source_folder="./build/windows/x64/runner/Release"
+# source_folder="./build/windows/runner/Release"
 output_zipfile="./bash/Mala.zip"
 # Compress-Archive -Path $source_folder -DestinationPath $output_zipfile
-powershell.exe -Command "Compress-Archive -Path './build/windows/runner/Release' -DestinationPath './bash/Mala.zip'"
+powershell.exe -Command "Compress-Archive -Path './build/windows/x64/runner/Release' -DestinationPath './bash/Mala.zip'"
